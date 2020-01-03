e05d95a10b1ad4fa108b1ed7dc85116d58aadc50
Florian Krull <flo.krull@gmail.com>
2020-01-03 14:35:54 +0100

    fix indentation

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,31 @@ int main(int argc, char const *argv[])
     return EXIT_FAILURE;
   }
   const char *text = argv[1];                  // User-supplied text
-	enum qrcodegen_Ecc errCorLvl = qrcodegen_Ecc_MEDIUM; // Error correction level
-	
-	// Make and print the QR Code symbol
-	uint8_t qrcode[qrcodegen_BUFFER_LEN_MAX];
-	uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
-	bool ok = qrcodegen_encodeText(text, tempBuffer, qrcode, errCorLvl,
-		qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
+  enum qrcodegen_Ecc errCorLvl = qrcodegen_Ecc_MEDIUM; // Error correction level
+  
+  // Make and print the QR Code symbol
+  uint8_t qrcode[qrcodegen_BUFFER_LEN_MAX];
+  uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
+  bool ok = qrcodegen_encodeText(text, tempBuffer, qrcode, errCorLvl,
+    qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
 
   if (!ok) {
     fputs("error encoding string\n", stderr);
     return EXIT_FAILURE;
   }
 
-	int size = qrcodegen_getSize(qrcode);
-	int border = 4;
+  int size = qrcodegen_getSize(qrcode);
+  int border = 4;
 
   fputs(text, stdout);
   fputs("\n", stdout);
-	for (int y = -border; y < size + border; y++) {
-		for (int x = -border; x < size + border; x++) {
-			fputs((qrcodegen_getModule(qrcode, x, y) ? "##" : "  "), stdout);
-		}
-		fputs("\n", stdout);
-	}
-	fputs("\n", stdout);
+  for (int y = -border; y < size + border; y++) {
+    for (int x = -border; x < size + border; x++) {
+      fputs((qrcodegen_getModule(qrcode, x, y) ? "##" : "  "), stdout);
+    }
+    fputs("\n", stdout);
+  }
+  fputs("\n", stdout);
   int scale = 5;
   int img_out_w = (size + border * 2) * scale;
   cimg_library::CImg<uint8_t> img_out(
